aae0744c4e86c167202b8bfb84b2373b0263a76a
supaiku <supaiku@protonmail.com>
2018-03-31 03:05:15 +0200

    Limit requestAnimationFrame calls by FPS
    Signed-off-by: supaiku <supaiku@protonmail.com>

--- src/client/classes/GameClient.ts
+++ src/client/classes/GameClient.ts
@@ -7,6 +7,11 @@ export default class GameClient extends AbstractGameClient {
     private clientLog: HTMLTextAreaElement
     private context2D: CanvasRenderingContext2D
 
+    private fpsLimiter: { fpsInterval: number, last: number } = {
+        fpsInterval: 1000 / Globals.FPS,
+        last: Date.now()
+    }
+
     constructor(public nickname: string, public connection?: WebSocket) {
         super(nickname, connection)
 
@@ -18,59 +23,66 @@ export default class GameClient extends AbstractGameClient {
         this.tick()
     }
 
-tick() {
-    this.update()
-    this.draw()
+    tick() {
+        window.requestAnimationFrame(() => this.tick());
 
-    window.requestAnimationFrame(() => this.tick());
-}
+        const now = Date.now()
+        const elapsed = now - this.fpsLimiter.last
 
-draw() {
-    this.drawRect(Globals.CANVAS_SIZE, { x: 0, y: 0 }, 'rgb(100, 100, 100)')
+        if (elapsed > this.fpsLimiter.fpsInterval) {
+            this.fpsLimiter.last = now - (elapsed % this.fpsLimiter.fpsInterval)
 
-    if (this.players) {
-        Array.from(this.players.values()).forEach((p: PlayerData, index) => {
-
-            this.drawPlayer(p)
-
-        })
-    }
-}
-
-private drawPlayer(playerData: PlayerData) {
-    const color = playerData.color
-
-    // Draw border around the own player
-    if (playerData.id === this.player.id) {
-        this.drawRect(playerData.size, playerData.position, 'white')
-
-        let innerPos = { x: playerData.position.x + 2, y: playerData.position.y + 2 }
-        let innerSize = { width: playerData.size.width - 4, height: playerData.size.height - 4 }
-        this.drawRect(innerSize, innerPos, color)
-
-    } else {
-        this.drawRect(playerData.size, playerData.position, color)
+            this.update()
+            this.draw()
+        }
     }
 
-    this.context2D.font = "11px Arial";
-    this.context2D.fillStyle = 'rgb(0, 0, 0)'
-    this.context2D.fillText(`${playerData.id}`, playerData.position.x + 4, playerData.position.y + 15);
-}
+    draw() {
+        this.drawRect(Globals.CANVAS_SIZE, { x: 0, y: 0 }, 'rgb(100, 100, 100)')
 
-private drawRect(size: { width: number, height: number }, pos: { x: number, y: number }, color: string) {
-    this.context2D.fillStyle = color
-    this.context2D.fillRect(pos.x, pos.y, size.width, size.height)
-}
+        if (this.players) {
+            Array.from(this.players.values()).forEach((p: PlayerData, index) => {
 
-log(message?: any) {
-    let text
-    if (typeof message == 'object') {
-        text = (JSON && JSON.stringify ? JSON.stringify(message) : message)
-    } else {
-        text = message
+                this.drawPlayer(p)
+
+            })
+        }
     }
 
-    this.clientLog.innerHTML += `[${new Date(Date.now()).toLocaleString()} ${this.nickname}] ${text}\n`
-}
+    private drawPlayer(playerData: PlayerData) {
+        const color = playerData.color
+
+        // Draw border around the own player
+        if (playerData.id === this.player.id) {
+            this.drawRect(playerData.size, playerData.position, 'white')
+
+            let innerPos = { x: playerData.position.x + 2, y: playerData.position.y + 2 }
+            let innerSize = { width: playerData.size.width - 4, height: playerData.size.height - 4 }
+            this.drawRect(innerSize, innerPos, color)
+
+        } else {
+            this.drawRect(playerData.size, playerData.position, color)
+        }
+
+        this.context2D.font = "11px Arial";
+        this.context2D.fillStyle = 'rgb(0, 0, 0)'
+        this.context2D.fillText(`${playerData.id}`, playerData.position.x + 4, playerData.position.y + 15);
+    }
+
+    private drawRect(size: { width: number, height: number }, pos: { x: number, y: number }, color: string) {
+        this.context2D.fillStyle = color
+        this.context2D.fillRect(pos.x, pos.y, size.width, size.height)
+    }
+
+    log(message?: any) {
+        let text
+        if (typeof message == 'object') {
+            text = (JSON && JSON.stringify ? JSON.stringify(message) : message)
+        } else {
+            text = message
+        }
+
+        this.clientLog.innerHTML += `[${new Date(Date.now()).toLocaleString()} ${this.nickname}] ${text}\n`
+    }
 
 }
--- src/shared/Globals.ts
+++ src/shared/Globals.ts
@@ -1,5 +1,6 @@
 export default class Globals {
 
+    public static readonly FPS = 60
     public static readonly CANVAS_WIDTH = 640
     public static readonly CANVAS_HEIGHT = 480
 
